171e6cc2678fb37a7f8877f04b0442196d590c2e
xuantruong262 <vxtruong262@gmail.com>
2025-11-10 22:09:43 +0700

    udpate dynamic lib test

---
+++ other_knowledge/dynamic_lib/cal.h
@@ -0,0 +1,8 @@
+#ifndef _CAL_H_
+#define _CAL_H
+
+
+int plus(int a, int b);
+
+
+#endif
---
+++ other_knowledge/dynamic_lib/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "cal.h"
+
+int main(){
+  int total = plus(5,4);
+  printf("Total: %d\n", total);
+  return 0;
+}
+
+
